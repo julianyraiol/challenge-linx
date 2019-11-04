b7abce1543a392719e9658fc13478fa360e2c22b
julianyraiol <juliany@motorola.com>
2019-11-04 17:19:47 -0400

    fix: Trie did not recognized special characters
    * changed array structure for map
    * updated insert method
    * updated print method

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # challange-linx
+
+## Instalation
+
+Requires a [json library](https://linux.tips/programming/how-to-install-and-use-json-cpp-library-on-ubuntu-linux-os)
--- challenge.cpp
+++ challenge.cpp
@@ -1,35 +1,31 @@
 #include <bits/stdc++.h>
-#define ALPHABET_SIZE 127
+#include <jsoncpp/json/json.h>
 
 using namespace std;
  
 struct Node{
-    struct Node* children[ALPHABET_SIZE];
-    char caracter;
+    map<wchar_t, Node*> children;
+    string id;
     bool finalized;
 
     Node(){
-        memset(children, false, sizeof(children));
         finalized = false;
     }
 };
 
-void insert(Node *root, string word){
+void insert(Node *root, string word, string id){
     
+    cout<< word << " == " << id << endl;
     int index;
-    for(char letter:word){
-        index = letter;
-        
-        if(!root->children[index]){
-            root->children[index] = new Node();
-            root->children[index]->caracter = letter;
-            
+    for(wchar_t letter:word){
+        if(root->children.find(letter) == root->children.end()){
+            root->children[letter] = new Node();      
         }
-        root = root->children[index];
+        root = root->children[letter];
     }
+    root->id = id;
     root->finalized = true;
 }
-
 
 void print(Node* root, string str, int &cont) 
 {   
@@ -37,10 +33,10 @@ void print(Node* root, string str, int &cont)
         cout << str << endl; 
     } 
 
-    for (int i = 0; i < ALPHABET_SIZE; i++){ 
-        if (root->children[i])  { 
-            str.push_back(root->children[i]->caracter); 
-            print(root->children[i], str, cont);
+    for (auto x:root->children){ 
+        if (x.second)  { 
+            str.push_back((x.first)); 
+            print(x.second, str, cont);
             str.pop_back();    
         } 
     } 
@@ -62,22 +58,25 @@ int print_by_prefix(Node* root, string word){
 }
 
 int main(){
+
     Node* root = new Node();
-    string str;
+    string str, str1;
     int cont = 0;
+    
     Json::Reader reader;
     Json::Value json_line;
     ifstream file_json("catalogo_produtos.json");
-
+  
     if(file_json){
         while (getline(file_json, str)){
             reader.parse(str, json_line);
-            cout << json_line << endl;
             insert(root, json_line["name"].asString(), json_line["id"].asString());
         }
     }   
     else{
         cout << "Erro ao abrir o arquivo" << endl;
-    }    
-    //print(root, str, cont);
+    }   
+    
+    cout << endl << "==============" << endl;
+    print(root, str1, cont);
 }
