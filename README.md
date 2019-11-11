e825333b01ab49c72af454a58c07d7eef81cd549
julianyraiol <juliany@motorola.com>
2019-11-10 22:51:28 -0400

    docs: Added "Exemplo de Consulta" section
    * update list_all_products_by_prefix method

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# challange-linx
+# Linx Challange 
 
 Projeto implementado utilizando a linguagem de programação C++ e aplicando o algoritmo de busca de padrões em strings KMP (Knuth-Morris-Pratt). 
-
-
 
 ### Instalação
 Para a leitura do arquivo Json, foi empregado a biblioteca [SimpleJSON](https://github.com/nbsdx/SimpleJSON).
@@ -14,8 +12,37 @@ $ cd challenge-linx
 
 ## Executar
 ```bash
-$ g++ kmp_version.cpp -o p; ./p
+$ g++ index.cpp -o processador; ./processador
+$ Digite aqui sua consulta: 
 ```
+
+## Exemplo de Consulta
+```bash
+$ Digite aqui sua consulta: Notebook
+
+#1 - 16298 - Notebook Acer Predator Triton 700, Intel® Core™ i7-7700HQ, 32GB, SSD 512GB, NVIDIA® GeForce® GTX 1080 - PT715-51-77DD
+#2 - 16688 - Notebook Acer Aspire 5, Intel® Core™ i7-7500U, 8GB, 1TB, Tela de 15,6'', Nvidia GeForce
+#3 - 2147447116 - Notebook Acer® Aspire 3, Intel® Core™ i5-7200U, 4GB, 1TB, Tela de 15,6\", Intel® HD Graphics 620 - A315-53-55DD
+#4 - 2147448565 - Notebook Acer, Intel CoreT i5-7200U, 4GB, 1TB, Tela de 15,6\", Intel® HD Graphics 620, Aspire 3 - A315-53-5100
+#5 - 2147449890 - Notebook Acer, Intel Core i5-7200U, 4GB, 1TB, Tela de 15,6\", Gráficos HD Intel® 620, Preto - Aspire 5 - A515-51-58DG
+#6 - 2147450193 - Notebook Acer, Intel Core i3-8130U, 4GB, 1TB, Tela de 15,6\", Gráficos HD Intel® 620 - Aspire 5 - A515-51-37LG
+#7 - 2147453060 - Notebook Acer Aspire 3, AMD Ryzen™ 3 2200U, 4GB, 1TB, Tela de 15.6”, Preto - A315-41-R790
+#8 - 2147453303 - Notebook Acer®, Intel Core i7-8550U, 8GB, 1TB, Tela de 15,6\", Cinza - Aspire 5 - A515-51-C0ZG
+#9 - 2147453304 - Notebook Acer®, Intel Core i7-8550U, 8GB, 1TB, Tela de 15,6\", Gráficos HD Intel® 620, Preto - Aspire 5 - A515-51-C2TQ
+#10 - 2147453305 - Notebook Acer® Spin 3, Intel® Core™ i7-8550U, 8GB, 256GB SSD, Tela de 14\", Intel® UHD Graphics 620 - SP314-51-C3ZZ
+#11 - 2147453400 - Notebook Acer, Intel® Core i5-8265U, 16GB, 1TB, Tela de 15,6\", Prata, Aspire 5 - A515-52G-57NL
+#12 - 2147453825 - Notebook Asus, Intel® Core™ i3-7020U, 4GB, 1TB, Tela 15,6\", Cinza - X543UA-GO2196T
+#13 - 2147453827 - Notebook Asus VivoBook 15, Intel® Core™ i5-8265U, 4GB, 1TB, Tela de 15,6\", Cinza Escuro - X512FA-BR566T
+#14 - 2147453829 - Notebook Asus VivoBook 15, Intel® Core i5, 8GB, 1TB, Tela de 15,6\", Prata - X512FA-BR569T
+#15 - 2147453832 - Notebook Asus, Intel® Celeron® N4000, 4GB, 500GB, Tela de 15.6\", Cinza Escuro - X543MA-GO594T
+#16 - 2147453833 - Notebook Asus, Intel® Celeron® N4000, 4GB, 500GB, Tela de 15.6\", Prata Metálico - X543MA-GO595T
+#17 - 2147453835 - Notebook Asus, Intel® Core™ i3 7020U, 4GB, 1TB, Tela de 15,6\", Cinza - X543UA-GO2194T
+#18 - 2147454447 - Notebook Aspire 3 Acer, Intel® Core™ i3-7020U, 4GB, 1TB, Tela de 15.6\" - A315-53-333H
+#19 - 2147454467 - Notebook Aspire 3 Acer®, AMD Ryzen™ 3-2200U, 4GB, 1TB, Tela de 15,6'' - A315-41-R790 + Mochila Green
+#20 - 2147454515 - Notebook Acer®, Intel Core i5-7200U, 4GB, 1TB, Tela de 15.6'', Gráficos HD Intel® 620, Aspire 5 - A515-51-52M7
+
+```
+
 
 
 
--- index.cpp
+++ index.cpp
@@ -1,237 +1,152 @@
 #include <iostream>
-#include <map>
+#include <vector>
 #include <string>
 #include <fstream>
-#include <jsoncpp/json/json.h>
+#include <algorithm> 
+#include "json.hpp"
 
 using namespace std;
+using json::JSON;
 
-class Node{
+
+class KMP{
     private:
-        map<wchar_t, Node*> children;
-        bool finalized_word;
-        string id_product;
+        vector<int> state;
+        string text;
+        string pattern;
     public:
-        Node();        
-
-        map<wchar_t, Node*> get_children();
-        
-        bool has_key(wchar_t);
-        void insert_node(wchar_t);
-        Node* get_node(wchar_t);
-
-        bool get_finalized_word();
-        void set_finalized_word(bool);
-
-        string get_id_product();
-        void set_id_product(string);   
+        KMP(string, string);
+        void state_process();
+        int search_pattern();
 };
 
-Node::Node(){
-    this->finalized_word = false;
+KMP::KMP(string text, string pattern){
+    this->text = text;
+    this->pattern = pattern;
+    this->state.assign(this->text.size(), 0);
 }
 
-map<wchar_t, Node*> Node::get_children(){
-    return this->children;
-}
+void KMP::state_process(){
+    int i = 0, j = -1, size_text, size_pattern;
+    size_text = this->text.size();
+    size_pattern = this->pattern.size();
 
-bool Node::has_key(wchar_t key){
-    return !(this->children.find(key) == this->children.end());
-}
-
-void Node::insert_node(wchar_t key){
-    this->children[key] = new Node();
-}
-
-Node* Node::get_node(wchar_t key){
-    return this->children[key];
-}
-
-void Node::set_finalized_word(bool finalized_word){
-    this->finalized_word = finalized_word;
-}
-
-bool Node::get_finalized_word(){
-    return this->finalized_word;
-}
-
-void Node::set_id_product(string id_product){
-    this->id_product = id_product;
-}
-
-string Node::get_id_product(){
-    return this->id_product;
-}
-
-class Trie{
-    private:
-        Node* root;
-        void print_all_elements(Node*, string, int&);
-    public:
-        Trie();
-        void add_element(string, string);
-        void show_all_occurences();
-        void show_occurences_by_prefix(string);
-};
-
-Trie::Trie(){
-    this->root = new Node();
-}
-
-void Trie::add_element(string product, string id_product){
-
-    Node* aux = this->root;
-
-    for(wchar_t letter:product){
-        if(!aux->has_key(letter)){
-            aux->insert_node(letter);
-        }
-        aux = aux->get_node(letter);
+    this->state[0] = -1;
+    while (i < size_pattern){
+        while(j >= 0 && this->pattern[i] != this->pattern[j])j = this->state[j];
+        i++; j++;
+        this->state[i] = j;
     }
-
-    aux->set_finalized_word(true);
-    aux->set_id_product(id_product);
 }
 
-void Trie::print_all_elements(Node* current_node,string current_word, int &all_occurences){
-    
-    // // if(all_occurences >= 20) return;
-
-    if(current_node->get_finalized_word()){
-        all_occurences++;
-        cout << "#" << all_occurences << " - " << current_node->get_id_product() << " - " << current_word << endl;
-    }
-
-    for(auto node:current_node->get_children()){
-        if(node.second){
-            current_word.push_back((node.first)); 
-            print_all_elements(node.second, current_word, all_occurences);
-            current_word.pop_back(); 
+int KMP::search_pattern(){
+    int i = 0, j = 0;
+    int size_text = this->text.size();
+    int size_pattern = this->pattern.size();
+   
+    while (i < size_text){
+        while(j >= 0 && this->text[i] != this->pattern[j]){
+            j = this->state[j];
+        } 
+        i++; 
+        j++;
+        if(j == size_pattern){
+            return i - j;
         }
     }
-}
-
-void Trie::show_all_occurences(){
-    string current_word;
-    int all_occurences=0;
-    if(!this->root->get_children().empty()){
-        print_all_elements(this->root, current_word, all_occurences);
-    }
-    else
-        cout << "Nenhum produto foi inserido." << endl;
-}
-
-void Trie::show_occurences_by_prefix(string prefix){
-
-    Node* aux = this->root;
-    int all_occurences = 0;
-
-    if(!aux->get_children().empty()){
-        for(wchar_t key:prefix){
-            if(!aux->has_key(key))
-                return;
-            aux = aux->get_node(key);
-        }
-        print_all_elements(aux, prefix, all_occurences);
-    }
-    else
-        cout << "Nenhum produto foi inserido." << endl;
+    return -1;
 }
 
 class Processing{
     private:
-        Trie* prefix_products;
+        map<string, string> list_products;
         string path_json;
+
+        void print_product(int, string, string);
     public:
         Processing(string);
         void read_products();
         void list_all_products();
         void list_all_products_by_prefix(string);
-        void main_process();
 };
 
 Processing::Processing(string path_json){
     this->path_json = path_json;
-    this->prefix_products = new Trie();
+}
+
+void Processing::print_product(int occurences, string id_product, string name_product){
+    cout << "#" << occurences << " - " << id_product << " - " << name_product << endl;
 }
 
 void Processing::read_products(){
-    Json::Reader reader;
-    Json::Value json_line;
+
     string str_line, name_product, id_product;
 
     ifstream file_json(this->path_json);
     if(file_json.is_open()){
             while (getline(file_json, str_line)){
 
-            reader.parse(str_line, json_line);
+            JSON Obj = JSON::Load(str_line);
 
-            id_product = json_line["id"].asString();
-            name_product = json_line["name"].asString();
-            
-            this->prefix_products->add_element(name_product, id_product);
+            id_product = Obj["id"].ToString();
+            name_product = Obj["name"].ToString();
+
+            this->list_products[id_product] = name_product;
         }
     }
     else
         cout << "Falha ao abrir arquivo Json" << endl;
 }
 
-void Processing::list_all_products(){
-    this->prefix_products->show_all_occurences();
-}
+void Processing::list_all_products_by_prefix(string pattern){
+    
+    int occurences = 1, search_result;
+    bool is_finish = false;
+    map<string, string> others_occurences;
 
-void Processing::list_all_products_by_prefix(string product_name){
+    for(auto product:this->list_products){
+        KMP *kmp = new KMP(product.second, pattern);
+        kmp->state_process();
 
-    if(!product_name.empty()){
-        this->prefix_products->show_occurences_by_prefix(product_name);
+        search_result = kmp->search_pattern();
+
+        if(search_result == 0)
+            print_product(occurences++, product.first, product.second);
+        else if(search_result != -1)
+            others_occurences[product.first] = product.second;
+
+        if(occurences > 20) return;
     }
-    else
-        cout << "Nenhum produto especificado para consulta." << endl;
+
+    for(auto product:others_occurences){
+        print_product(occurences++, product.first, product.second);
+        if(occurences > 20) return;
+    }
 }
 
-void Processing::main_process(){
-    string product_name;
-    string message = "1- Listar todos os produtos \n2- Listar por tipo de produto\n3- Sair";
-    
-    int option;
-    bool is_running = true;
-    
-    this->read_products();
-    
-    while (is_running){
-        cout << endl << message << endl;
-        cin >> option;
-
-        switch (option){
-            case 1:
-                this->prefix_products->show_all_occurences();
-                break;
-            case 2:
-                cout << "\nDigite aqui sua consulta: ";
-                cin >> product_name;
-                this->prefix_products->show_occurences_by_prefix(product_name); 
-                break;
-            case 3:
-                is_running = false;
-                break;
-            default:
-                cout << "Insira uma opção correta." << endl;
-                break;
-        }
+void Processing::list_all_products(){
+    int i = 1;
+    for(auto product:this->list_products){
+        print_product(i++, product.first, product.second);
     }
 }
 
 int main(){
 
     string PATH_JSON = "catalogo_produtos.json";
-    string product_name;
     Processing process = Processing(PATH_JSON);
-    
-    process.read_products();
-    
-    cout << "Digite sua consulta: ";
-    getline(cin, product_name);
-    process.list_all_products_by_prefix(product_name);
+    string product_name;
 
+    process.read_products();
+
+    while(true){
+        cout << "Digite aqui sua consulta: ";
+        getline(cin, product_name);
+         
+        process.list_all_products_by_prefix(product_name);
+       
+        cout << endl;
+    }
+    
     return 0;
-}
+} 
